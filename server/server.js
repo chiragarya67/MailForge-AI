1229d54cd9d2a45a3a8521ee1bdeed2d27829723
chirag <chiragarya5678@gmail.com>
2026-08-12 20:39:10 +0530

    Serve static files and handle all routes

--- server/server.js
+++ server/server.js
@@ -16,6 +16,12 @@ app.use(express.urlencoded({ extended : "true"}));
 app.use(cors({
    origin: process.env.FRONTEND_URL || true,
 }))
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/dist", "index.html"));
+});
 
 app.get("/", (req,res)=>{
    res.send("Server is working!")
